46c815ed42066a753f47d0c7e38f5c6b9a597c94
PaulMart85 <outimer@mail.ru>
2023-05-25 20:58:31 +0600

    seminar_1 : compilation and interpretation using docker

--- seminar1/src/main/java/sample/Client.java
+++ seminar1/src/main/java/sample/Client.java
@@ -2,6 +2,45 @@ package sample;
 
 import regular.*;
 
+/**
+ https://www.docker.com/products/docker-desktop/
+ https://hub.docker.com/
+
+ javac -sourcepath ./java  -d out java/sample/Client.java
+ *
+ java -classpath ./out sample.Client
+ *
+ javadoc -encoding utf8 -d docs -sourcepath ./java -cp ./out -subpackages regular
+ * */
+
+/**
+
+ FROM bellsoft/liberica-openjdk-alpine
+ COPY ./java ./src
+ RUN mkdir ./out
+ RUN javac -sourcepath ./src -d out src/sample/Client.java
+ CMD  java -classpath ./out sample.Client
+
+ FROM bellsoft/liberica-openjdk-alpine as BuildProject
+ WORKDIR /app
+ COPY ./java ./src
+ RUN mkdir ./out
+ RUN javac -sourcepath ./src -d out ./src/sample/Client.java
+
+ FROM scratch as OutputFiles
+ COPY --from=BuildProject /app/out /bin
+
+ >> docker buildx build --output type=local,dest=. .
+
+ FROM bellsoft/liberica-openjdk-alpine
+ WORKDIR /app
+ COPY ./bin .
+ CMD java -classpath . sample.Client
+
+ >> docker build . -t runjavaapp:v1
+ >> docker run runjavaapp:v1
+
+ */
 public class Client {
 
     public static void main(String[] args) {
